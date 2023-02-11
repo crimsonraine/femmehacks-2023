0e1de2e7170aa282a496cf80ca80065273d5089e
Kimberley Ni <wukimberley98@gmail.com>
2023-02-11 11:13:57 -0500

    maybe useful for amazon specific

--- background.js
+++ background.js
@@ -1,12 +1,25 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes("https://www.amazon.com/")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
+      const queryParameters = tab.url.split("com/")[1];
+      // const urlParameters = new URLSearchParams(queryParameters);
   
       chrome.tabs.sendMessage(tabId, {
         type: "NEW",
-        videoId: urlParameters.get("v"),
+        item_desc_url: getItem(tab.url),
+        // random: "random"
       });
     }
   });
+
+function getItem (url) {
+    var beginning = 23;
+    var ending = beginning + 1;
+    for (let i = beginning; i < tab.url.length; i++) {
+        if (tab.url.substring(i, i+1).equals("/")) {
+            ending = i;
+            break;
+        }
+    }
+    return tab.url.substring(beginning, ending);
+}
   
