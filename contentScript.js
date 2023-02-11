1e2113d92ad7359e2dae5ef0960c1c2ff1865f8b
Kimberley Ni <wukimberley98@gmail.com>
2023-02-11 14:58:43 -0500

    ASIN works?

--- contentScript.js
+++ contentScript.js
@@ -5,8 +5,8 @@
     let current_inventory = [];
 
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const {type, value, item_desc_url} = obj;
-
+        const { type, value, item_desc_url } = obj;
+        console.log('running');
         if (type === "NEW") {
             current_item = item_desc_url;
             newItemLoaded();
@@ -15,11 +15,11 @@
 
     const fetch_inventory = () => {
         return new Promise((resolve) => {
-          chrome.storage.sync.get([current_item], (obj) => {
-            resolve(obj[current_item] ? JSON.parse(obj[current_item]) : []);
-          });
+            chrome.storage.sync.get([current_item], (obj) => {
+                resolve(obj[current_item] ? JSON.parse(obj[current_item]) : []);
+            });
         });
-      };
+    };
 
     const newItemLoaded = async () => {
         const saveButtonExists = document.getElementsByClassName("save-btn")[0];
@@ -39,8 +39,15 @@
     }
 
     const addNewItemEventHandler = async () => {
+        const activeTab = window.location.href;
+        const queryParameters = activeTab.split("com/")[1];
+        const urlParameters = new URLSearchParams(queryParameters);
+
+        const current_item = getASIN(activeTab);
+
         const newItem = {
-            asin: amazon_title.textContent,
+            asin: getASIN(activeTab),
+            title: amazon_title.textContent,
             desc: 'NA'
         }
         current_inventory = await fetch_inventory();
@@ -48,6 +55,15 @@
         chrome.storage.sync.set({
             [current_item]: JSON.stringify([...current_inventory, newItem])
         });
+
+        function getASIN(url_asin) {
+            var beginning = 23;
+            for (let i = beginning; i < url_asin.length - 1; i++) {
+                if (url_asin.substring(i, i + 3)==="dp/") {
+                    return url_asin.substring(i + 3, i + 13)
+                }
+            }
+        }
     }
 
     newItemLoaded();
